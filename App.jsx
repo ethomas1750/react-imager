e0fa7da79f7fcfae2afb1408c2dacb8fdf0789e8
ethomas1750 <144511038+ethomas1750@users.noreply.github.com>
2024-06-09 22:10:24 -0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,1 +1,14 @@
+import React from 'react'
+import './App.css'
+import Imager from './components/Imager'
 
+
+function App() {
+  return (
+    <>
+    <Imager/>
+    </>
+  )
+}
+
+export default App
